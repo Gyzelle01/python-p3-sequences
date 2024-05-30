5fd2c7c71cadb85e5a7dd28d417cf61d61e31d22
Gyzelle <gyznjeri@gmail.com>
2024-05-30 07:27:26 +0300

    python-p3-sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacc_sequence = []
+
+    if length == 0:
+        fibonacc_sequence = []
+    
+    elif length == 1:
+        fibonacc_sequence = [0]
+
+    elif length == 2:
+        fibonacc_sequence = [0, 1]
+
+    else:
+        fibonacc_sequence = [0, 1]
+        for i in range(2, length):
+            fibonacc_sequence.append(fibonacc_sequence[i - 1] + fibonacc_sequence[i - 2])
+
+   
+    print(fibonacc_sequence)
